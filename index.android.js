a002c8d87a81e44372b6a88dff82077237f0cb3a
sreeja <sreejagpt@gmail.com>
2016-06-09 14:45:28 +1000

    added a toggle mode

--- index.android.js
+++ index.android.js
@@ -5,9 +5,9 @@ import {
   Text,
   View,
   ListView,
+  Image,
+  Switch,
 } from 'react-native';
-
-import SideMenu from 'react-native-side-menu';
 
 class ABCGoodNews extends Component {
 
@@ -18,6 +18,7 @@ class ABCGoodNews extends Component {
       articles : new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
       }),
+      happyModeOn: false,
     };
   }
 
@@ -38,16 +39,29 @@ class ABCGoodNews extends Component {
   }
 
   render() {
-    var menu = <Menu navigator={navigator}/>;
     return (
-      <View style={styles.container}>
-        <ListView
-          dataSource={this.state.articles}
-          renderRow={(article) => <Text style = {styles.row}>{article.short_description}</Text>}
-        />
 
-      </View>
+        <View style={styles.container}>
+          <View style={styles.horizontal}>
+            <Text>Happy Mode {this.isHappyModeEnabled()}</Text>
+            <Switch
+            onValueChange={(value) => this.setState({happyModeOn: value})}
+            value={this.state.happyModeOn} />
+          </View>
+
+
+          <ListView
+            dataSource={this.state.articles}
+            renderRow={(article) => <Text style = {styles.row}>{article.short_description}</Text>}
+          />
+
+        </View>
+
     );
+  }
+
+  isHappyModeEnabled() {
+    return (this.state.happyModeOn === true) ? 'ON' : 'OFF';
   }
 }
 
@@ -55,12 +69,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    paddingLeft: 10,
   },
   row: {
     backgroundColor: '#F0F0F0F0',
     margin: 10,
+  },
+  settings: {
+    width: 50,
+    height: 50,
   },
 });
 
